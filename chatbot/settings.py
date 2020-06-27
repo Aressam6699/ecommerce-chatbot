495cdb75d065cb26df9ad96a49712d913f339c0e
Samarth Nair <54450141+Aressam6699@users.noreply.github.com>
2020-06-27 15:08:55 +0530

    Update settings.py

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xnk=ep-gknvn5egt4e6gf8aj(7zx$hpc7m%$_pldeo@m-ai%-9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
